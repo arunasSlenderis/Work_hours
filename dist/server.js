3d4b119758547c5b36eec3cef42e355e29f400c5
Arunas Slenderis <nasas60@gmail.com>
2017-04-12 11:12:08 +0300

    ...

--- dist/server.js
+++ dist/server.js
@@ -2543,7 +2543,7 @@ if(process.env.NODE_ENV.trim() == "production") {
   // app.use(express.static(path.join(__dirname, "dist")));
 
   app.get("/*", (req, res) => {
-    res.sendFile(__WEBPACK_IMPORTED_MODULE_8_path___default.a.join(__dirname, "dist", "index.html"));
+    res.sendFile(__WEBPACK_IMPORTED_MODULE_8_path___default.a.join(__dirname, "index.html"));
   });
   // app.get("/*", function (req, res, next) {
   //   const filename = path.join(__dirname, "index.html");
